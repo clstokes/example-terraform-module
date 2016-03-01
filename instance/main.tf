42d6af771895a83cc3c4b373a0c7e4459d77dc17
Cameron Stokes <cameron@cameronstokes.com>
2016-02-29 18:35:29 -0800

    Add tags to instance.

--- instance/main.tf
+++ instance/main.tf
@@ -4,6 +4,10 @@ variable "instance_type" {
 resource "aws_instance" "main" {
   ami           = "ami-9a562df2"
   instance_type = "${var.instance_type}"
+
+  tags {
+    Name = "terraform-module-example-${var.instance_type}"
+  }
 }
 
 output "instance_id" {
