c55ffe49d4ee49f5dc4ebef7e1c05ebdf275b382
Cameron Stokes <cameron@cameronstokes.com>
2016-02-29 18:35:06 -0800

    Add slash to module source.

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "small_instance" {
-  source = "instance"
+  source = "./instance"
 
   instance_type = "t2.micro"
 }
 
 module "medium_instance" {
-  source = "instance"
+  source = "./instance"
 
   instance_type = "c3.large"
 }
